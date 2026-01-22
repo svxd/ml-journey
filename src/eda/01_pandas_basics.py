a2472e9bc0cfaac87576ba75dedef321f4998232
SV <svxd.info@gmail.com>
2026-01-22 21:25:09 +0300

    init: project structure + pandas basics

--- src/eda/01_pandas_basics.py
+++ src/eda/01_pandas_basics.py
@@ -10,12 +10,20 @@ df = pd.DataFrame(data)
 # print(df)
 
 
-print(df.head(2))
-print(f"{'---------' * 3}")
-print(df.shape)
-print(f"{'---------' * 3}")
-df.info()
-print(f"{'---------' * 3}")
-print(df.mean())
-print(f"{'---------' * 3}")
-print(df.loc[1, "salary"])
+# print(df.head(2))
+# print(f"{'---------' * 3}")
+# print(df.shape)
+# print(f"{'---------' * 3}")
+# df.info()
+# print(f"{'---------' * 3}")
+# print(df.mean())
+# print(f"{'---------' * 3}")
+# print(df.loc[1, "salary"])
+
+
+print(df[df["salary"] > 80000]) 
+print(df[(df["age"] > 35) & (df["experience"] >= 10)])
+
+print(df.sort_values("salary", ascending=False))
+
+
